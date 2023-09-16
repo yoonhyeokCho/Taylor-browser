6047311536f0ff3616cecbf3196f1e78dbc59b0e
yoonhyeokCho <hyeok6640@gmail.com>
2023-09-17 01:57:15 +0900

    [feat] 바를 이용한 크기 조절 기능 추가

--- src/screen/MainHome.js
+++ src/screen/MainHome.js
@@ -1,16 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, DeviceEventEmitter } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, DeviceEventEmitter, Animated, PanResponder } from "react-native";
 import Browser from "../components/Browser/Browser";
 import RenderSafeAreaView from "../components/Layout/RenderSafeAreaView";
 
 const MainHome = () => {
-    //newScreenValue = true 면 브라우저 추가, false면 구글 화면 추가
-
+    const pan = useRef(new Animated.ValueXY()).current;
     const [isSplit, setIsSplit] = useState(true);
+    const [splitContainer1Height, setSplitContainer1Height] = useState(300);
+    const [splitContainer2Height, setSplitContainer2Height] = useState(500);
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => true,
+        
+        onPanResponderMove: (e, gesture) => {
+            const newHeight1 = Math.max(100, splitContainer1Height + gesture.dy);
+            const newHeight2 = Math.max(100, splitContainer2Height - gesture.dy);
+
+            setSplitContainer1Height(newHeight1);
+            setSplitContainer2Height(newHeight2);
+        },
+        
+        onPanResponderRelease: () => {
+            pan.flattenOffset();
+        },
+    });
 
     useEffect(() => {
         console.log('booleanValue2 changed:', isSplit);
-        DeviceEventEmitter.addListener("splitView",(isSplit) => {
+        DeviceEventEmitter.addListener("splitView", (isSplit) => {
             setIsSplit(isSplit);
         });
     }, []);
@@ -18,14 +36,14 @@ const MainHome = () => {
     return (
         <RenderSafeAreaView>
             <View style={styles.entire}>
-                <View style={isSplit ? styles.splitContainer1 : styles.fullContainer}>
+                <View style={{ height: splitContainer1Height }}>
                     <Browser />
                 </View>
 
                 {isSplit && (
                     <View>
-                        <View style={styles.grayBar} />
-                        <View style={styles.splitContainer2}>
+                        <View style={styles.grayBar} {...panResponder.panHandlers} />
+                        <View style={{ height: splitContainer2Height }}>
                             <Browser />
                         </View>
                     </View>
@@ -38,24 +56,12 @@ const MainHome = () => {
 const styles = StyleSheet.create({
     entire: {
         height: '100%',
-        backgroundColor: 'yellow',
-    },
-    fullContainer: {
-        height: 750,
-        backgroundColor: 'green',
-    },
-    splitContainer1: {
-        height: 300,
-        backgroundColor: 'green',
-    },
-    splitContainer2: {
-        height: 500,
         backgroundColor: 'white',
     },
     grayBar: {
         width: '100%',
-        height: 10,
-        backgroundColor: 'gray',
+        height: 20,
+        backgroundColor: '#206FE5',
     }
 })
 
