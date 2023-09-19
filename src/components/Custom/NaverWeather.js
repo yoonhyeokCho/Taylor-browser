6869de572faa03c23663b5e63280d1502f956954
yoonhyeokCho <hyeok6640@gmail.com>
2023-09-19 16:35:17 +0900

    [add] 네이버, 다음, 구글 각 영역 추출

---
+++ src/components/Custom/NaverWeather.js
@@ -0,0 +1,28 @@
+//MM_HOME_SEARCH_WEATHER id
+
+import React, { useEffect, useRef } from "react";
+import { View } from "react-native";
+import WebView from 'react-native-webview';
+import { useRemoveExcept } from "../hooks/useRemoveExcept";
+
+
+const NaverStock = () => {
+    const WebViewRef = useRef();
+
+    useRemoveExcept(WebViewRef, "MM_HOME_SEARCH_WEATHER", "id");
+
+    return (
+        <View>
+            <WebView
+                ref={WebViewRef}
+                source={{ uri: 'https://naver.com' }}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}>
+
+            </WebView>
+        </View>
+
+    )
+}
+
+export default NaverStock
